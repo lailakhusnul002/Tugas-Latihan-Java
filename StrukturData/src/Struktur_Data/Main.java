4db10739f76bbd9d2bbcb3d755c086d2054ec6e4
lailakhusnul002 <74228918+lailakhusnul002@users.noreply.github.com>
2020-11-11 14:07:32 +0700

    Add files via upload

---
+++ StrukturData/src/Struktur_Data/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Struktur_Data;
+
+/**
+ *
+ * @author USER
+ */
+import Struktur_Data.kamera;
+import Struktur_Data.laptop;
+import Struktur_Data.smartphone;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+ 
+        kamera.jumlahBaterai();
+        laptop.jumlahBaterai();
+        smartphone.jumlahBaterai();
+        
+        
+    }
+}
+
---
+++ StrukturData/src/Struktur_Data/laptop.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Struktur_Data;
+
+/**
+ *
+ * @author USER
+ */
+public class laptop {
+    public static void jumlahBaterai(){
+        System.out.println("Baterai 7");
+}
+    }

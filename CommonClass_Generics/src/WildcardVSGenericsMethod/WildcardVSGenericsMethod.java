39990a557d409cb44e3f5e47149b408abe35aed2
lailakhusnul002 <74228918+lailakhusnul002@users.noreply.github.com>
2021-01-13 17:59:20 -1200

    Add files via upload

---
+++ CommonClass_Generics/src/WildcardVSGenericsMethod/WildcardVSGenericsMethod.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WildcardVSGenericsMethod;
+
+import java.util.ArrayList;
+import java.util.Collection;
+/**
+ *
+ * @author USER
+ */
+
+ 
+public class WildcardVSGenericsMethod {
+    // mengunakan wildcards
+    static void print(Collection<?> col) {
+        for (Object o : col) {
+            System.out.println(o);
+        }
+    }
+    
+    // menggunakan generic methods
+    static <T> Collection arrayToCollection(T[] a) {
+        Collection<T> c = new ArrayList();
+        for (T o : a) {
+            c.add(o);
+        }
+        return c;
+    }
+ 
+    public static void main(String[] args) {
+        String[] sa = { "Happy", "Coding" };
+        Collection r = arrayToCollection(sa);
+        print(r);
+    }
+}
